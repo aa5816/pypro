0f4230a4e2e4ac9f477a5baf3266a3db50fa50b3
ANMOL AHUJA <500061317@DDN.UPES.AC.IN>
2018-06-11 15:20:03 +0530

    1st program

--- py1.py
+++ py1.py
@@ -1,16 +1,11 @@
-#takes customer info of 3 customers and prints it
-i=0
-while (i<3):
-  print("for customer :", i+1)
-  cust_id=int(input("enter 4 digit the customer id"))
-  bill_id=int(input("enter 3 digit bill id"))
-  if_minor=bool(input("is it a minor enter False or True"))
-  cust_name=str(input("enter customer name"))
-  bill_amt=float(input("enter bill amount"))
-  i+=1
+#takes customer info of a customer and prints it
+print("for customer :")
+cust_id=int(input("enter 4 digit the customer id"))
+bill_id=int(input("enter 3 digit bill id"))
+if_minor=bool(input("is it a minor enter False or True"))
+cust_name=str(input("enter customer name"))
+bill_amt=float(input("enter bill amount"))
+print ("customer info is",'\n',cust_id,'\n',bill_id,'\n',if_minor,'\n',cust_name,'\n',bill_amt,'\n')
 
-while (i>0):
-  print ("customerc info is",cust_id,'\n',bill_id,'\n',if_minor,'\n',cust_name,'\n',bill_amt,'\n')
-  i-=1
 
   
